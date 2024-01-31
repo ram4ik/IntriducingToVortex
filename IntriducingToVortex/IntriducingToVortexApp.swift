c683e76f0639118903aca5de1f92948662f9b7a3
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-31 19:52:37 +0200

    Initial Commit

---
+++ IntriducingToVortex/IntriducingToVortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntriducingToVortexApp.swift
+//  IntriducingToVortex
+//
+//  Created by test on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IntriducingToVortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
