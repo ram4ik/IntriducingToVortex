23b59e4b7024e3c901edc3251db336c805eb072e
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-31 20:15:00 +0200

    init

--- IntriducingToVortex/ContentView.swift
+++ IntriducingToVortex/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
+import Vortex
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ZStack {
+                VStack {
+                    Text("RAIN")
+                        .opacity(0.3)
+                        .font(.largeTitle)
+                        .bold()
+                }
+                VortexView(.splash) {
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 32)
+                        .tag("circle")
+                }
+                VortexView(.rain) {
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 32)
+                        .tag("circle")
+                }
+            }.tabItem {
+                Label("Rain", systemImage: "cloud.rain")
+            }
+            
+            VortexView(.fireworks) {
+                Circle()
+                    .fill(.white)
+                    .blendMode(.plusLighter)
+                    .frame(width: 32)
+                    .tag("circle")
+            }.tabItem {
+                Label("Fireworks", systemImage: "fireworks")
+            }
+            
+            VortexView(.fire) {
+                Circle()
+                    .fill(.white)
+                    .blendMode(.plusLighter)
+                    .blur(radius: 3)
+                    .frame(width: 32)
+                    .tag("circle")
+            }.tabItem {
+                Label("Fire", systemImage: "flame")
+            }
+            
+            VortexView(.snow) {
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 32)
+                    .tag("circle")
+            }.tabItem {
+                Label("Snow", systemImage: "snowflake")
+            }
+            
+            VortexView(.smoke) {
+                Circle()
+                    .fill(.gray)
+                    .frame(width: 23)
+                    .tag("circle")
+            }.tabItem {
+                Label("Smoke", systemImage: "fluid.transmission")
+            }
         }
-        .padding()
     }
 }
 
